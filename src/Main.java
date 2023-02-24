391e79a8446be9d0daa03558b0e45a2f0b963330
JoaMarinCardenas <l22390441@chetumal.tecnm.mx>
2023-02-24 11:20:42 -0500

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner Leer = new Scanner(System.in);
+
+        int num1, num2;
+        System.out.println("Digite el primer numero entero:");
+        num1 = Leer.nextInt();
+        System.out.println("Digite el segundo numero entero:");
+        num2 = Leer.nextInt();
+
+        double resta=num1-num2;
+
+         double divisor1=num1/resta;
+         double divisor2=num2/resta;
+
+
+
+    }
+
+    }
+
